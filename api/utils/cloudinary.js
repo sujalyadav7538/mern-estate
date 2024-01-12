0c09cd1403b2ad44cc6ce54957ba9bd4906a2c23
sujal <ashutoshsingh2920@gmail.com>
2024-01-12 18:08:16 +0530

    update the psswd

--- api/utils/cloudinary.js
+++ api/utils/cloudinary.js
@@ -2,9 +2,9 @@ import cloudinary from 'cloudinary';
 import fs from 'fs';
 
 cloudinary.v2.config({
-  cloud_name: 'dtypndhh4',
-  api_key: '145325432432977',
-  api_secret: 'zSwoFSDfumN_nAXjFB2sJVKmdxE',
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
