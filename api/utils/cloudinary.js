8d753f301abaece60ff7a469900cc7180b6894c9
sujal <ashutoshsingh2920@gmail.com>
2024-01-24 15:10:50 +0530

    ...

--- api/utils/cloudinary.js
+++ api/utils/cloudinary.js
@@ -6,7 +6,7 @@ cloudinary.v2.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret:process.env.CLOUDINARY_API_SECRET,
   secure: true,
-});
+})
 
 const uploadOnCloud = async (localFilePath) => {
   try {
